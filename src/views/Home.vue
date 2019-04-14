3930ce92fea1c77eb7eaa603299b599951467ca4
Taylor Tobin <19tt94@gmail.com>
2019-04-14 15:50:48 -0700

    added home view

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="home">
+    <slider :quotes="quotes" />
+  </div>
+</template>
+
+<script>
+import slider from "@/components/WordSlider";
+
+export default {
+  components: {
+    slider
+  },
+
+  data() {
+    return {
+      quotes: [
+        "Every generation has a story",
+        "New model original parts",
+        "A whole new kind of animal",
+        "Question reality. Change your destiny.",
+        "Hero…Legend…King",
+        "Anyone can save the galaxy…once.",
+        "Bring the adventure…to life",
+        "Don’t let anyone spoil this."
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
