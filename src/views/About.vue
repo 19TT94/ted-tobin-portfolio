b9bf96a60d7c1873b1357f2c3b27991959a4f5fd
Taylor Tobin <19tt94@gmail.com>
2019-04-19 22:44:35 -0700

    added about and scripts page

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="about">
+    <p class="bio">
+      I have had the privilege of working on some of the biggest films of all time. (Star Wars, Avengers, Jungle Book, Beauty and The Beast, Black Panther and The Fast and Furious) I have brought together teams of talented creative people who made me look good. (Created the Disney In-house marketing department) I am now returning to my roots as a copywriter as well as helping out both studios and vendors alike as a creative consultant. I am proud of where I have been and excited about where I am going.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "@/assets/scss/app.scss"; // global styles
+
+#about {
+  padding: 5rem 2rem 0;
+  color: $orange;
+  background: $black;
+  min-height: 100vh;
+
+  .bio {
+    line-height: 30px;
+  }
+}
+</style>
