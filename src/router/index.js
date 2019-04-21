60aa61084fedb2a14644436ee692df4c7d6c69cc
Taylor Tobin <19tt94@gmail.com>
2019-04-21 12:01:24 -0700

    added 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/views/Home'
 import About from '@/views/About'
 import Scripts from '@/views/Scripts'
+import NotFound from '@/views/NotFound'
 
 Vue.use(Router)
 
@@ -23,6 +24,12 @@ export default new Router({
       path: '/scripts',
       name: 'Scripts',
       component: Scripts
+    },
+    {
+      // will match everything
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="not-found">
+    <p class="bio">
+      404
+    </p>
+    <router-link class="item desktop" to="/">Back to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "@/assets/scss/app.scss"; // global styles
+
+#not-found {
+  padding: 0 2rem;
+  color: $orange;
+  background: $black;
+  min-height: 100%;
+  @include flexbox(column, nowrap, center, center);
+}
+</style>
